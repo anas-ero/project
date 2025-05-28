21cc45f3127bdae17cc79f3d74895af0cfdfa964
Anas <anassanda111@gmail.com>
2025-05-28 21:31:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const dababa = document.getElementById("dababao")
 console.log("gr")
 console.log("GREW")
 
-
+// js a test qwith aa comment
 
 
 const brr = document.getElementById("brr")
 
 
 console.log("Brb br br batapim")
+
+const databaas = document.getElementById("dabaass")
+
+var databass = document.getElementById("var")
